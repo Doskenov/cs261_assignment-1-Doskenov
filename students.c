5167d4dd0f0a08098e3682294de328ea3f66ffa3
Bakhtiyar Doskenov <43548655+Doskenov@users.noreply.github.com>
2018-10-14 17:26:30 -0700

    Update students.c

--- students.c
+++ students.c
@@ -123,7 +123,7 @@ struct dynarray* create_student_array(int num_students, char** names, int* ids,
 void free_student_array(struct dynarray* students) {
  int i;
  for (i=0; i<8; i++) {
-   free_student(dynarray_get(students, p));
+   free_student(dynarray_get(students, i));
  }
  dynarray_free(students);
 
